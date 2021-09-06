48266b32f65d8ecdba61dbf83dca1a553e89445e
Nikola Irinchev <irinchev@me.com>
2021-09-06 12:10:04 +0200

    Re-run ncc

--- release-to-slack/dist/index.js
+++ release-to-slack/dist/index.js
@@ -7,25 +7,6 @@ require('./sourcemap-register.js');module.exports =
 
 "use strict";
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -33,7 +14,6 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getPayload = void 0;
 const slackify_markdown_1 = __importDefault(__webpack_require__(9418));
 const moment_1 = __importDefault(__webpack_require__(9623));
-const core = __importStar(__webpack_require__(2186));
 const sectionsRegex = /### (?<sectionName>[^\r\n]*)(?<sectionContent>.+?(?=###|$))/gs;
 function getPayload(changelog, sdk, repoUrl, version) {
     sectionsRegex.lastIndex = 0;
@@ -64,9 +44,6 @@ class SlackPayload {
         this.blocks.push(new DividerBlock());
     }
     addSection(title, text) {
-        core.info("---------------");
-        core.info(title);
-        core.info("---------------");
         this.blocks.push(new SectionBlock(`*${title}*`));
         this.blocks.push(new SectionBlock(slackify_markdown_1.default(text)));
     }
