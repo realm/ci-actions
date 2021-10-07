3275135be5c514914eac8270de96fe51361975c0
Nikola Irinchev <irinchev@me.com>
2021-10-07 17:58:16 +0200

    A bit of a cleanup

--- mdb-realm/__tests__/helpers.spec.ts
+++ mdb-realm/__tests__/helpers.spec.ts
@@ -1,13 +1,12 @@
 import { expect } from "chai";
 import "mocha";
 import { suite, test } from "@testdeck/mocha";
-import { createCluster } from "../src/helpers";
 
 @suite
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class helpersTests {
     @test
-    testPayloadGenerator(): void {
+    exampleTest(): void {
         expect(true).to.be.true;
     }
 }
